eff790a5031a64a5804a90e172fb6fb933b87a85
Md Ali Arman <79654408+aliarman1@users.noreply.github.com>
2023-08-26 11:15:14 +0600

    Update rgbimgallchannel.m

--- Rep2/rgbimgallchannel.m
+++ Rep2/rgbimgallchannel.m
@@ -5,14 +5,18 @@ redChannel = rgbImage(:,:,1);
 greenChannel = rgbImage(:,:,2);
 blueChannel = rgbImage(:,:,3);
 
-subplot(1, 3, 1);
+subplot(2, 2, 1);
+imshow(rgbImage);
+title('Original Image');
+
+subplot(2, 2, 2);
 imshow(redChannel);
 title('Red Channel');
 
-subplot(1, 3, 2);
+subplot(2, 2, 3);
 imshow(greenChannel);
 title('Green Channel');
 
-subplot(1, 3, 3);
+subplot(2, 2, 4);
 imshow(blueChannel);
 title('Blue Channel');
